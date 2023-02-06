377340b3a69f2dbf64512b670a2678f5e96ebaef
Ahmed Alpanna <alpanna@gmail.com>
2023-02-06 17:35:12 +0200

    3rd

--- UI/Controllers/OrderHeadsController.cs
+++ UI/Controllers/OrderHeadsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Domain.Models;
+using Repos.RepoMain;
 
 namespace UI.Controllers
 {
